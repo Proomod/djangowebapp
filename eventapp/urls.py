6e646ade35146f76f5fd767ffdbe2a474efa90d7
Pramod <bhusalpramod456@gmail.com>
2019-09-23 11:49:02 +0545

    Second commit

--- eventapp/urls.py
+++ eventapp/urls.py
@@ -4,10 +4,10 @@ from eventapp import views
 app_name = "gallery"
 
 urlpatterns = [
-    path("", views.index, name="upcoming"),
+    path("gallery/", views.index, name="gallery"),
     path("post/", views.post, name="eventpost"),
     path("event_detail/<int:id>/", views.event_detail, name="eventdetail"),
-    path("upcomingevents/", views.upcoming_events, name="upcoming"),
+    path("", views.upcoming_events, name="upcoming"),
     path("completedevents/", views.completed_events, name="completed"),
     path("delete/<int:id>/", views.deletepost, name="delete"),
     path("eventapp/add_members/<int:id>", views.add_members, name="addmember"),
